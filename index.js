54dbd8b71b6f09dec89e7661ed7c3392930156e8
madhurima999 <gorainmadhurima999@gmail.com>
2023-10-18 17:15:16 +0530

    f

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const Movie = require("./models/Movie");
 const axios = require("axios");
-const port=3000
+const port=3000 
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/movies-recomm")
